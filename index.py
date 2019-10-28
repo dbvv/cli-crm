7613f652e1446e36ad1516d961171cb5dad018f4
Daniil Borovkov <daniilborowkow@gmail.com>
2019-10-28 16:50:09 +0600

    frontend initial setup

--- index.py
+++ index.py
@@ -1,7 +1,11 @@
 from flask import Flask, json
 import vk_parser
 
-api = Flask(__name__)
+api = Flask(__name__, static_folder="web/dist", static_url_path='')
+
+@api.route("/", methods=['GET'])
+def index():
+  return api.send_static_file('index.html')
 
 @api.route("/vk/groups", methods=['GET'])
 def get_groups():
