9c218d5974f845a1bc1c8cbd2ed3e970fbed6e43
Daniil Borovkov <daniilborowkow@gmail.com>
2020-01-12 16:27:57 +0600

    Added sms bumper

--- web/src/components/ContactsLit.vue
+++ web/src/components/ContactsLit.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="contact-list">
+    <v-row justify="center">
+      <v-dialog v-model="dialog" persistent max-width="290">
+        <template v-slot:activator="{ on }">
+          <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
+        </template>
+        <v-card>
+          <v-card-title class="headline">Use Google's location service?</v-card-title>
+          <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
+            <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
+
     <v-simple-table>
       <template v-slot:default>
         <thead>
@@ -29,7 +46,8 @@
         name: 'ContactsList',
         data: () => {
           return {
-          contacts: []
+            contacts: [],
+            dialog: false,
           }
         },
       mounted() {
